b03c36c94df5872f43b285d263db3d221b7a4145
Andreas Guther <andreas@guther.net>
2017-11-25 20:02:49 +0100

    Added example for routing service started as library

---
+++ src/main/java/com/github/aguther/dds/examples/routing/Routing.java
@@ -0,0 +1,74 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Andreas Guther
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.aguther.dds.examples.routing;
+
+import com.rti.routingservice.RoutingService;
+import com.rti.routingservice.RoutingServiceProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Routing {
+
+  private static final Logger log;
+
+  private static boolean shouldTerminate;
+
+  static {
+    log = LoggerFactory.getLogger(Routing.class);
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+
+    // register shutdown hook
+    registerShutdownHook();
+
+    // setup routing service properties
+    final RoutingServiceProperty routingServiceProperty = new RoutingServiceProperty();
+    routingServiceProperty.cfgFile = "routing.xml";
+    routingServiceProperty.serviceName = "dds-examples-routing";
+    routingServiceProperty.applicationName = routingServiceProperty.serviceName;
+
+    // create routing service instance
+    try (RoutingService routingService = new RoutingService(routingServiceProperty)) {
+
+      // start routing service
+      routingService.start();
+
+      while (!shouldTerminate) {
+        Thread.sleep(1000);
+      }
+
+      // stop routing service
+      routingService.stop();
+    }
+  }
+
+  private static void registerShutdownHook() {
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      log.info("Shutdown signal received...");
+      shouldTerminate = true;
+    }));
+  }
+}
