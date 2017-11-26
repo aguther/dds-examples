6f275b5e1ee35573a3cb6670a13ecab0d9c39f70
Andreas Guther <andreas@guther.net>
2017-11-26 11:11:58 +0100

    Switched to more simple reading of samples

--- src/main/java/com/github/aguther/dds/examples/discovery/PublicationObserver.java
+++ src/main/java/com/github/aguther/dds/examples/discovery/PublicationObserver.java
@@ -25,16 +25,12 @@
 package com.github.aguther.dds.examples.discovery;
 
 import com.rti.dds.domain.DomainParticipant;
-import com.rti.dds.infrastructure.ResourceLimitsQosPolicy;
-import com.rti.dds.publication.builtin.PublicationBuiltinTopicDataDataReader;
-import com.rti.dds.publication.builtin.PublicationBuiltinTopicDataSeq;
+import com.rti.dds.infrastructure.RETCODE_NO_DATA;
+import com.rti.dds.publication.builtin.PublicationBuiltinTopicData;
 import com.rti.dds.publication.builtin.PublicationBuiltinTopicDataTypeSupport;
 import com.rti.dds.subscription.DataReader;
 import com.rti.dds.subscription.InstanceStateKind;
 import com.rti.dds.subscription.SampleInfo;
-import com.rti.dds.subscription.SampleInfoSeq;
-import com.rti.dds.subscription.SampleStateKind;
-import com.rti.dds.subscription.ViewStateKind;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -63,43 +59,31 @@ class PublicationObserver extends BuiltinTopicObserver {
 
   @Override
   public void on_data_available(DataReader dataReader) {
-    // cast the data reader to a typed one
-    PublicationBuiltinTopicDataDataReader publicationDataReader = (PublicationBuiltinTopicDataDataReader) dataReader;
 
-    // create data containers
-    PublicationBuiltinTopicDataSeq publicationDataSeq = new PublicationBuiltinTopicDataSeq();
-    SampleInfoSeq sampleInfoSeq = new SampleInfoSeq();
+    boolean hasMoreData = true;
 
-    // take data from the data reader
-    publicationDataReader.take(
-        publicationDataSeq,
-        sampleInfoSeq,
-        ResourceLimitsQosPolicy.LENGTH_UNLIMITED,
-        SampleStateKind.ANY_SAMPLE_STATE,
-        ViewStateKind.ANY_VIEW_STATE,
-        InstanceStateKind.ANY_INSTANCE_STATE);
+    do {
+      try {
+        // create data containers
+        PublicationBuiltinTopicData sample = new PublicationBuiltinTopicData();
+        SampleInfo sampleInfo = new SampleInfo();
 
-    // iterate over data received
-    for (int i = 0; i < publicationDataSeq.size(); i++) {
-      // get sample info
-      SampleInfo sampleInfo = sampleInfoSeq.get(i);
+        // read next sample
+        dataReader.read_next_sample_untyped(sample, sampleInfo);
 
-      // when data is not valid, continue
-      if (!sampleInfo.valid_data) {
-        if (sampleInfo.instance_state == InstanceStateKind.NOT_ALIVE_DISPOSED_INSTANCE_STATE
-            && log.isInfoEnabled()) {
-          log.info("Disposed: {}", sampleInfo.instance_handle.toString());
+        if (sampleInfo.valid_data) {
+          if (log.isInfoEnabled()) {
+            log.info("Discovered: {}", sampleInfo.instance_handle.toString());
+          }
+        } else if (sampleInfo.instance_state == InstanceStateKind.NOT_ALIVE_DISPOSED_INSTANCE_STATE) {
+          if (log.isInfoEnabled()) {
+            log.info("Disposed  : {}", sampleInfo.instance_handle.toString());
+          }
         }
-        continue;
+      } catch (RETCODE_NO_DATA exception) {
+        hasMoreData = false;
       }
 
-      // print instance handle that was discovered
-      if (log.isInfoEnabled()) {
-        log.info("Discovered: {}", sampleInfo.instance_handle.toString());
-      }
-    }
-
-    // return loan
-    publicationDataReader.return_loan(publicationDataSeq, sampleInfoSeq);
+    } while (hasMoreData);
   }
 }
