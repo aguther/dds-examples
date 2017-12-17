2fb545fab4634465ebbaab3f828bf330fbab64a0
Andreas Guther <andreas@guther.net>
2017-12-17 20:03:55 +0100

    Added discovery participant to routing example

--- src/main/java/com/github/aguther/dds/examples/routing/Routing.java
+++ src/main/java/com/github/aguther/dds/examples/routing/Routing.java
@@ -24,6 +24,17 @@
 
 package com.github.aguther.dds.examples.routing;
 
+import com.github.aguther.dds.examples.discovery.PublicationObserver;
+import com.github.aguther.dds.examples.discovery.PublicationObserverListener;
+import com.rti.dds.domain.DomainParticipant;
+import com.rti.dds.domain.DomainParticipantFactory;
+import com.rti.dds.domain.DomainParticipantFactoryQos;
+import com.rti.dds.domain.DomainParticipantQos;
+import com.rti.dds.infrastructure.InstanceHandle_t;
+import com.rti.dds.infrastructure.PartitionQosPolicy;
+import com.rti.dds.infrastructure.StatusKind;
+import com.rti.dds.publication.builtin.PublicationBuiltinTopicData;
+import com.rti.dds.publication.builtin.PublicationBuiltinTopicDataTypeSupport;
 import com.rti.routingservice.RoutingService;
 import com.rti.routingservice.RoutingServiceProperty;
 import org.slf4j.Logger;
@@ -55,6 +66,54 @@ public class Routing {
 
       // start routing service
       routingService.start();
+
+      // disable auto enable
+      DomainParticipantFactoryQos domainParticipantFactoryQos = new DomainParticipantFactoryQos();
+      DomainParticipantFactory.get_instance().get_qos(domainParticipantFactoryQos);
+      domainParticipantFactoryQos.entity_factory.autoenable_created_entities = false;
+      DomainParticipantFactory.get_instance().set_qos(domainParticipantFactoryQos);
+
+      // create another participant
+      DomainParticipantQos domainParticipantQos = new DomainParticipantQos();
+      DomainParticipant domainParticipant = DomainParticipantFactory.get_instance().create_participant(
+          0,
+          DomainParticipantFactory.PARTICIPANT_QOS_DEFAULT,
+          null,
+          StatusKind.STATUS_MASK_NONE
+      );
+      domainParticipant.get_qos(domainParticipantQos);
+      domainParticipantQos.participant_name.name = "Discovery";
+      domainParticipant.set_qos(domainParticipantQos);
+
+      // create new publication observer
+      PublicationObserver publicationObserver = new PublicationObserver(domainParticipant);
+      publicationObserver.addListener(new PublicationObserverListener() {
+        @Override
+        public void publicationDiscovered(
+            InstanceHandle_t instanceHandle,
+            PublicationBuiltinTopicData data
+        ) {
+          log.info(
+              "Discovered Publication : instance='{}', topic='{}', type='{}'",
+              instanceHandle.toString(),
+              data.topic_name,
+              data.type_name
+          );
+        }
+
+        @Override
+        public void publicationLost(
+            InstanceHandle_t instanceHandle
+        ) {
+          log.info(
+              "Lost Publication       : instance='{}'",
+              instanceHandle.toString()
+          );
+        }
+      });
+
+      // enable participant
+      domainParticipant.enable();
 
       while (!shouldTerminate) {
         Thread.sleep(1000);
