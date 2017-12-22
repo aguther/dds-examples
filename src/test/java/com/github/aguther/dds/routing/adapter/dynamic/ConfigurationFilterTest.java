d006deccb34dc0475ce3be3022a15a0d29803239
Andreas Guther <andreas@guther.net>
2017-12-22 14:32:05 +0100

    Improved unit tests

--- src/test/java/com/github/aguther/dds/routing/adapter/dynamic/ConfigurationFilterTest.java
+++ src/test/java/com/github/aguther/dds/routing/adapter/dynamic/ConfigurationFilterTest.java
@@ -1,11 +1,38 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Andreas Guther
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.github.aguther.dds.routing.adapter.dynamic;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import com.github.aguther.dds.routing.dynamic.observer.Session;
+import com.github.aguther.dds.routing.dynamic.observer.TopicRoute;
+import com.github.aguther.dds.routing.dynamic.observer.TopicRoute.Direction;
 import com.rti.dds.publication.builtin.PublicationBuiltinTopicData;
 import com.rti.dds.subscription.builtin.SubscriptionBuiltinTopicData;
-import java.util.Properties;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,53 +42,8 @@ public class ConfigurationFilterTest {
 
   @Before
   public void setUp() throws Exception {
-    // create properties
-    Properties properties = new Properties();
-    properties.put(
-        "dynamic_routing_adapter.administration.domain_id",
-        "0"
-    );
-    properties.put(
-        "dynamic_routing_adapter.administration.discovery.wait_time",
-        "10000"
-    );
-    properties.put(
-        "dynamic_routing_adapter.discovery.domain_id",
-        "0"
-    );
-    properties.put(
-        "dynamic_routing_adapter.configuration.domain_route_name",
-        "Default"
-    );
-    properties.put(
-        "dynamic_routing_adapter.configuration.Shape.allow_topic_name_filter",
-        "Square|.*"
-    );
-    properties.put(
-        "dynamic_routing_adapter.configuration.Shape.deny_topic_name_filter",
-        "Circle"
-    );
-    properties.put(
-        "dynamic_routing_adapter.configuration.Shape.allow_partition_name_filter",
-        "A|B|C|D"
-    );
-    properties.put(
-        "dynamic_routing_adapter.configuration.Shape.deny_partition_name_filter",
-        ".*\\*"
-    );
-    properties.put(
-        "dynamic_routing_adapter.configuration.Shape.qos.topic_route",
-        "<publish_with_original_info>true</publish_with_original_info>"
-    );
-    properties.put("dynamic_routing_adapter.configuration.Shape.qos.input",
-        "<datareader_qos/>"
-    );
-    properties.put("dynamic_routing_adapter.configuration.Shape.qos.output",
-        "<datawriter_qos/>"
-    );
-
     // create configuration filter
-    configurationFilter = new ConfigurationFilter(properties);
+    configurationFilter = new ConfigurationFilter(PropertyFactory.create());
   }
 
   @Test
@@ -70,12 +52,13 @@ public class ConfigurationFilterTest {
 
     publicationBuiltinTopicData.topic_name = "Square";
     assertFalse(configurationFilter.ignorePublication(null, null, publicationBuiltinTopicData));
+    publicationBuiltinTopicData.topic_name = "Triangle";
+    assertFalse(configurationFilter.ignorePublication(null, null, publicationBuiltinTopicData));
 
     publicationBuiltinTopicData.topic_name = "Circle";
     assertTrue(configurationFilter.ignorePublication(null, null, publicationBuiltinTopicData));
-
-    publicationBuiltinTopicData.topic_name = "Triangle";
-    assertFalse(configurationFilter.ignorePublication(null, null, publicationBuiltinTopicData));
+    publicationBuiltinTopicData.topic_name = "OtherTopic";
+    assertTrue(configurationFilter.ignorePublication(null, null, publicationBuiltinTopicData));
   }
 
   @Test
@@ -84,43 +67,129 @@ public class ConfigurationFilterTest {
 
     subscriptionBuiltinTopicData.topic_name = "Square";
     assertFalse(configurationFilter.ignoreSubscription(null, null, subscriptionBuiltinTopicData));
+    subscriptionBuiltinTopicData.topic_name = "Triangle";
+    assertFalse(configurationFilter.ignoreSubscription(null, null, subscriptionBuiltinTopicData));
 
     subscriptionBuiltinTopicData.topic_name = "Circle";
     assertTrue(configurationFilter.ignoreSubscription(null, null, subscriptionBuiltinTopicData));
-
-    subscriptionBuiltinTopicData.topic_name = "Triangle";
-    assertFalse(configurationFilter.ignoreSubscription(null, null, subscriptionBuiltinTopicData));
   }
 
   @Test
   public void ignorePartition() {
+    assertFalse(configurationFilter.ignorePartition("A"));
+    assertFalse(configurationFilter.ignorePartition("ATest"));
+    assertFalse(configurationFilter.ignorePartition("B"));
+
+    assertTrue(configurationFilter.ignorePartition("*"));
+    assertTrue(configurationFilter.ignorePartition("A*"));
+    assertTrue(configurationFilter.ignorePartition("D"));
+    assertTrue(configurationFilter.ignorePartition("D*"));
+
+    assertTrue(configurationFilter.ignorePartition("C"));
   }
 
   @Test
   public void getSessionParent() {
+    assertEquals(
+        PropertyFactory.CONFIGURATION_DOMAIN_ROUTE_NAME,
+        configurationFilter.getSessionParent(new Session("Square", "A"))
+    );
   }
 
   @Test
   public void getSessionName() {
+    assertEquals(
+        "Square()",
+        configurationFilter.getSessionName(new Session("Square", ""))
+    );
+    assertEquals(
+        "Square(A)",
+        configurationFilter.getSessionName(new Session("Square", "A"))
+    );
   }
 
   @Test
   public void getSessionEntityName() {
+    assertEquals(
+        String.format(
+            "%s::%s",
+            PropertyFactory.CONFIGURATION_DOMAIN_ROUTE_NAME,
+            "Square()"
+        ),
+        configurationFilter.getSessionEntityName(new Session("Square", ""))
+    );
+    assertEquals(
+        String.format(
+            "%s::%s",
+            PropertyFactory.CONFIGURATION_DOMAIN_ROUTE_NAME,
+            "Square(A)"
+        ),
+        configurationFilter.getSessionEntityName(new Session("Square", "A"))
+    );
   }
 
   @Test
   public void getSessionConfiguration() {
+    Session session = new Session("Square", "A");
+    String configuration = configurationFilter.getSessionConfiguration(session);
+
+    assertTrue(configuration.contains("Square"));
+    assertTrue(configuration.contains("A"));
   }
 
   @Test
   public void getTopicRouteName() {
+    assertEquals(
+        Direction.IN.toString(),
+        configurationFilter.getTopicRouteName(
+            new Session("Square", ""),
+            new TopicRoute(Direction.IN, "Square", "ShapeType")
+        )
+    );
+    assertEquals(
+        Direction.OUT.toString(),
+        configurationFilter.getTopicRouteName(
+            new Session("Square", ""),
+            new TopicRoute(Direction.OUT, "Square", "ShapeType")
+        )
+    );
   }
 
   @Test
   public void getTopicRouteEntityName() {
+    assertEquals(
+        String.format(
+            "%s::%s::%s",
+            PropertyFactory.CONFIGURATION_DOMAIN_ROUTE_NAME,
+            "Square()",
+            Direction.IN.toString()
+        ),
+        configurationFilter.getTopicRouteEntityName(
+            new Session("Square", ""),
+            new TopicRoute(Direction.IN, "Square", "ShapeType")
+        )
+    );
+    assertEquals(
+        String.format(
+            "%s::%s::%s",
+            PropertyFactory.CONFIGURATION_DOMAIN_ROUTE_NAME,
+            "Square()",
+            Direction.OUT.toString()
+        ),
+        configurationFilter.getTopicRouteEntityName(
+            new Session("Square", ""),
+            new TopicRoute(Direction.OUT, "Square", "ShapeType")
+        )
+    );
   }
 
   @Test
   public void getTopicRouteConfiguration() {
+    Session session = new Session("Square", "A");
+    TopicRoute topicRoute = new TopicRoute(Direction.OUT, session.getTopic(), "ShapeType");
+    String configuration = configurationFilter.getTopicRouteConfiguration(session, topicRoute);
+
+    assertTrue(configuration.contains("Square"));
+    assertTrue(configuration.contains("ShapeType"));
   }
 }
---
+++ src/test/java/com/github/aguther/dds/routing/adapter/dynamic/PropertyFactory.java
@@ -0,0 +1,93 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Andreas Guther
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.aguther.dds.routing.adapter.dynamic;
+
+import java.util.Properties;
+
+public class PropertyFactory {
+
+  public static final int ADMINISTRATION_DOMAIN_ID = 1;
+  public static final int DISCOVERY_DOMAIN_ID = 2;
+  public static final int ADMINISTRATION_DISCOVERY_WAIT_TIME = 10000;
+  public static final String CONFIGURATION_DOMAIN_ROUTE_NAME = "DomainRouteTest";
+
+  private PropertyFactory() {
+  }
+
+  public static Properties create() {
+    // create properties
+    Properties properties = new Properties();
+
+    // fill data
+    properties.put(
+        "dynamic_routing_adapter.administration.domain_id",
+        ADMINISTRATION_DOMAIN_ID
+    );
+    properties.put(
+        "dynamic_routing_adapter.administration.discovery.wait_time",
+        ADMINISTRATION_DISCOVERY_WAIT_TIME
+    );
+    properties.put(
+        "dynamic_routing_adapter.discovery.domain_id",
+        DISCOVERY_DOMAIN_ID
+    );
+    properties.put(
+        "dynamic_routing_adapter.configuration.domain_route_name",
+        CONFIGURATION_DOMAIN_ROUTE_NAME
+    );
+    properties.put(
+        "dynamic_routing_adapter.configuration.Shape.allow_topic_name_filter",
+        "Square|T.*"
+    );
+    properties.put(
+        "dynamic_routing_adapter.configuration.Shape.deny_topic_name_filter",
+        "Circle"
+    );
+    properties.put(
+        "dynamic_routing_adapter.configuration.Shape.allow_partition_name_filter",
+        "A.*|B"
+    );
+    properties.put(
+        "dynamic_routing_adapter.configuration.Shape.deny_partition_name_filter",
+        ".*\\*|D"
+    );
+    properties.put(
+        "dynamic_routing_adapter.configuration.Shape.qos.topic_route",
+        "<publish_with_original_info>true</publish_with_original_info>"
+    );
+    properties.put(
+        "dynamic_routing_adapter.configuration.Shape.qos.input",
+        "<datareader_qos/>"
+    );
+    properties.put(
+        "dynamic_routing_adapter.configuration.Shape.qos.output",
+        "<datawriter_qos/>"
+    );
+
+    // return result
+    return properties;
+  }
+
+}
