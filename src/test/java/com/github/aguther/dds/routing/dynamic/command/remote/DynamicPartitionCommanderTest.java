356b32e534a5ba31956072f98829e24096305b33
Andreas Guther <andreas@guther.net>
2018-01-28 21:32:54 +0100

    Moved and extended unit tests for remote commander

--- src/test/java/com/github/aguther/dds/routing/dynamic/command/remote/DynamicPartitionCommanderTest.java
+++ src/test/java/com/github/aguther/dds/routing/dynamic/command/remote/DynamicPartitionCommanderTest.java
@@ -22,38 +22,55 @@
  * SOFTWARE.
  */
 
-package com.github.aguther.dds.routing.dynamic.command;
+package com.github.aguther.dds.routing.dynamic.command.remote;
 
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.github.aguther.dds.routing.dynamic.command.DynamicPartitionCommandProvider;
 import com.github.aguther.dds.routing.dynamic.command.remote.DynamicPartitionCommander;
 import com.github.aguther.dds.routing.dynamic.observer.Session;
 import com.github.aguther.dds.routing.dynamic.observer.TopicRoute;
 import com.github.aguther.dds.routing.dynamic.observer.TopicRoute.Direction;
 import com.github.aguther.dds.routing.util.RoutingServiceCommandInterface;
+import idl.RTI.RoutingService.Administration.CommandKind;
 import idl.RTI.RoutingService.Administration.CommandRequest;
 import idl.RTI.RoutingService.Administration.CommandResponse;
 import idl.RTI.RoutingService.Administration.CommandResponseKind;
 import java.util.concurrent.TimeUnit;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.powermock.reflect.Whitebox;
 
 public class DynamicPartitionCommanderTest {
 
+  private CommandResponse commandResponseOk;
+  private CommandResponse commandResponseError;
+
   private RoutingServiceCommandInterface commandInterface;
   private DynamicPartitionCommandProvider commanderProvider;
+  private CommandBuilder commandBuilder;
 
   private DynamicPartitionCommander commander;
 
   @Before
   public void setUp() {
+    commandResponseOk = new CommandResponse();
+    commandResponseOk.kind = CommandResponseKind.RTI_ROUTING_SERVICE_COMMAND_RESPONSE_OK;
+
+    commandResponseError = new CommandResponse();
+    commandResponseError.kind = CommandResponseKind.RTI_ROUTING_SERVICE_COMMAND_RESPONSE_ERROR;
+
     commandInterface = mock(RoutingServiceCommandInterface.class);
     commanderProvider = mock(DynamicPartitionCommandProvider.class);
 
@@ -433,7 +450,7 @@ public class DynamicPartitionCommanderTest {
     when(commandInterface.createCommandRequest()).thenReturn(new CommandRequest());
     when(commandInterface.sendRequest(any(CommandRequest.class), anyLong(), any(TimeUnit.class)))
         .thenAnswer(invocation -> {
-          Thread.sleep(1000);
+          Thread.sleep(100);
           return null;
         });
     when(commanderProvider.getTopicRouteConfiguration(any(Session.class), any(TopicRoute.class)))
@@ -457,5 +474,298 @@ public class DynamicPartitionCommanderTest {
         .getTopicRouteConfiguration(session, topicRoute);
     verify(commandInterface, timeout(5000).times(1))
         .sendRequest(any(CommandRequest.class), anyLong(), any(TimeUnit.class));
+  }
+
+  @Test
+  @SuppressWarnings("squid:S2925")
+  // A -> run -> success -> end
+  public void testCaseFlowRunSuccess() {
+
+    // replace command builder with mock
+    CommandBuilder commandBuilder = mock(CommandBuilder.class);
+    Whitebox.setInternalState(commander, "commandBuilder", commandBuilder);
+
+    Session session = new Session("Square", "A");
+
+    CommandRequest commandRequestCreate = new CommandRequest();
+    commandRequestCreate.command._d = CommandKind.RTI_ROUTING_SERVICE_COMMAND_CREATE;
+    Command commandCreate = new Command(
+        CommandType.COMMAND_TYPE_CREATE,
+        session,
+        null,
+        commandRequestCreate,
+        "Logging"
+    );
+
+    when(commandBuilder.buildCreateSessionCommand(eq(session))).thenReturn(commandCreate);
+
+    when(commandInterface.sendRequest(
+        eq(commandRequestCreate),
+        anyLong(),
+        any(TimeUnit.class))
+    ).thenAnswer(invocation -> {
+      return commandResponseOk;
+    });
+
+    commander.createSession(session);
+
+    verify(commandInterface, timeout(5000).times(1))
+        .sendRequest(eq(commandRequestCreate), anyLong(), any(TimeUnit.class));
+  }
+
+  @Test
+  @SuppressWarnings("squid:S2925")
+  // A -> run -> failed -> wait retry -> run -> success -> end
+  public void testCaseFlowRunFailedWaitRetryRunSuccess() {
+    // replace command builder with mock
+    CommandBuilder commandBuilder = mock(CommandBuilder.class);
+    Whitebox.setInternalState(commander, "commandBuilder", commandBuilder);
+
+    Session session = new Session("Square", "A");
+
+    CommandRequest commandRequestCreate = new CommandRequest();
+    commandRequestCreate.command._d = CommandKind.RTI_ROUTING_SERVICE_COMMAND_CREATE;
+    Command commandCreate = new Command(
+        CommandType.COMMAND_TYPE_CREATE,
+        session,
+        null,
+        commandRequestCreate,
+        "Logging"
+    );
+
+    when(commandBuilder.buildCreateSessionCommand(eq(session))).thenReturn(commandCreate);
+
+    when(commandInterface.sendRequest(
+        eq(commandRequestCreate),
+        anyLong(),
+        any(TimeUnit.class))
+    ).thenAnswer(invocation -> {
+      return commandResponseError;
+    }).thenAnswer(invocation -> {
+      return commandResponseOk;
+    });
+
+    commander.createSession(session);
+
+    verify(commandInterface, timeout(5000).times(2))
+        .sendRequest(eq(commandRequestCreate), anyLong(), any(TimeUnit.class));
+  }
+
+  @Test
+  @SuppressWarnings("squid:S2925")
+  // A -> run -> failed -> wait retry -> run -> success -> end
+  public void testCaseFlowRunTimeoutWaitRetryRunSuccess() {
+    // replace command builder with mock
+    CommandBuilder commandBuilder = mock(CommandBuilder.class);
+    Whitebox.setInternalState(commander, "commandBuilder", commandBuilder);
+
+    Session session = new Session("Square", "A");
+
+    CommandRequest commandRequestCreate = new CommandRequest();
+    commandRequestCreate.command._d = CommandKind.RTI_ROUTING_SERVICE_COMMAND_CREATE;
+    Command commandCreate = new Command(
+        CommandType.COMMAND_TYPE_CREATE,
+        session,
+        null,
+        commandRequestCreate,
+        "Logging"
+    );
+
+    when(commandBuilder.buildCreateSessionCommand(eq(session))).thenReturn(commandCreate);
+
+    when(commandInterface.sendRequest(
+        eq(commandRequestCreate),
+        anyLong(),
+        any(TimeUnit.class))
+    ).thenAnswer(invocation -> {
+      return null;
+    }).thenAnswer(invocation -> {
+      return commandResponseOk;
+    });
+
+    commander.createSession(session);
+
+    verify(commandInterface, timeout(5000).times(2))
+        .sendRequest(eq(commandRequestCreate), anyLong(), any(TimeUnit.class));
+  }
+
+  @Test
+  @SuppressWarnings("squid:S2925")
+  // Case 3: A -> run -> failed -> wait retry -> B -> abort (A) -> run (B) -> success -> end
+  public void testCaseFlowRunFailedWaitRetryAbortRunSuccess() throws InterruptedException {
+    // replace command builder with mock
+    CommandBuilder commandBuilder = mock(CommandBuilder.class);
+    Whitebox.setInternalState(commander, "commandBuilder", commandBuilder);
+
+    Session session = new Session("Square", "A");
+
+    CommandRequest commandRequestCreate = new CommandRequest();
+    commandRequestCreate.command._d = CommandKind.RTI_ROUTING_SERVICE_COMMAND_CREATE;
+    Command commandCreate = new Command(
+        CommandType.COMMAND_TYPE_CREATE,
+        session,
+        null,
+        commandRequestCreate,
+        "Logging"
+    );
+
+    CommandRequest commandRequestDelete = new CommandRequest();
+    commandRequestDelete.command._d = CommandKind.RTI_ROUTING_SERVICE_COMMAND_DELETE;
+    Command commandDelete = new Command(
+        CommandType.COMMAND_TYPE_DELETE,
+        session,
+        null,
+        commandRequestDelete,
+        "Logging"
+    );
+
+    when(commandBuilder.buildCreateSessionCommand(eq(session))).thenReturn(commandCreate);
+    when(commandBuilder.buildDeleteSessionCommand(eq(session))).thenReturn(commandDelete);
+
+    when(commandInterface.sendRequest(
+        eq(commandRequestCreate),
+        anyLong(),
+        any(TimeUnit.class))
+    ).thenAnswer(invocation -> {
+      Thread.sleep(0);
+      return commandResponseError;
+    });
+    when(commandInterface.sendRequest(
+        eq(commandRequestDelete),
+        anyLong(),
+        any(TimeUnit.class))
+    ).thenAnswer(invocation -> {
+      Thread.sleep(0);
+      return commandResponseOk;
+    });
+
+    commander.createSession(session);
+    Thread.sleep(25);
+    commander.deleteSession(session);
+
+    verify(commandInterface, timeout(5000).times(1))
+        .sendRequest(eq(commandRequestCreate), anyLong(), any(TimeUnit.class));
+    verify(commandInterface, timeout(5000).times(1))
+        .sendRequest(eq(commandRequestDelete), anyLong(), any(TimeUnit.class));
+  }
+
+  @Test
+  @SuppressWarnings("squid:S2925")
+  // A -> run -> B -> abort (A) -> success (A) -> run (B) -> success (B) -> end
+  public void testCaseFlowRunAbortSuccessRunSuccess() throws InterruptedException {
+    // replace command builder with mock
+    CommandBuilder commandBuilder = mock(CommandBuilder.class);
+    Whitebox.setInternalState(commander, "commandBuilder", commandBuilder);
+
+    Session session = new Session("Square", "A");
+
+    CommandRequest commandRequestCreate = new CommandRequest();
+    commandRequestCreate.command._d = CommandKind.RTI_ROUTING_SERVICE_COMMAND_CREATE;
+    Command commandCreate = new Command(
+        CommandType.COMMAND_TYPE_CREATE,
+        session,
+        null,
+        commandRequestCreate,
+        "Logging"
+    );
+
+    CommandRequest commandRequestDelete = new CommandRequest();
+    commandRequestDelete.command._d = CommandKind.RTI_ROUTING_SERVICE_COMMAND_DELETE;
+    Command commandDelete = new Command(
+        CommandType.COMMAND_TYPE_DELETE,
+        session,
+        null,
+        commandRequestDelete,
+        "Logging"
+    );
+
+    when(commandBuilder.buildCreateSessionCommand(eq(session))).thenReturn(commandCreate);
+    when(commandBuilder.buildDeleteSessionCommand(eq(session))).thenReturn(commandDelete);
+
+    when(commandInterface.sendRequest(
+        eq(commandRequestCreate),
+        anyLong(),
+        any(TimeUnit.class))
+    ).thenAnswer(invocation -> {
+      Thread.sleep(75);
+      return commandResponseOk;
+    });
+    when(commandInterface.sendRequest(
+        eq(commandRequestDelete),
+        anyLong(),
+        any(TimeUnit.class))
+    ).thenAnswer(invocation -> {
+      Thread.sleep(0);
+      return commandResponseOk;
+    });
+
+    commander.createSession(session);
+    Thread.sleep(25);
+    commander.deleteSession(session);
+
+    verify(commandInterface, timeout(5000).times(1))
+        .sendRequest(eq(commandRequestCreate), anyLong(), any(TimeUnit.class));
+    verify(commandInterface, timeout(5000).times(1))
+        .sendRequest(eq(commandRequestDelete), anyLong(), any(TimeUnit.class));
+  }
+
+  @Test
+  @SuppressWarnings("squid:S2925")
+  // A -> run -> B -> abort (A) -> failed (A) -> end
+  public void testCaseFlowRunAbortFailed() throws InterruptedException {
+    // replace command builder with mock
+    CommandBuilder commandBuilder = mock(CommandBuilder.class);
+    Whitebox.setInternalState(commander, "commandBuilder", commandBuilder);
+
+    Session session = new Session("Square", "A");
+
+    CommandRequest commandRequestCreate = new CommandRequest();
+    commandRequestCreate.command._d = CommandKind.RTI_ROUTING_SERVICE_COMMAND_CREATE;
+    Command commandCreate = new Command(
+        CommandType.COMMAND_TYPE_CREATE,
+        session,
+        null,
+        commandRequestCreate,
+        "Logging"
+    );
+
+    CommandRequest commandRequestDelete = new CommandRequest();
+    commandRequestDelete.command._d = CommandKind.RTI_ROUTING_SERVICE_COMMAND_DELETE;
+    Command commandDelete = new Command(
+        CommandType.COMMAND_TYPE_DELETE,
+        session,
+        null,
+        commandRequestDelete,
+        "Logging"
+    );
+
+    when(commandBuilder.buildCreateSessionCommand(eq(session))).thenReturn(commandCreate);
+    when(commandBuilder.buildDeleteSessionCommand(eq(session))).thenReturn(commandDelete);
+
+    when(commandInterface.sendRequest(
+        eq(commandRequestCreate),
+        anyLong(),
+        any(TimeUnit.class))
+    ).thenAnswer(invocation -> {
+      Thread.sleep(75);
+      return commandResponseError;
+    });
+    when(commandInterface.sendRequest(
+        eq(commandRequestDelete),
+        anyLong(),
+        any(TimeUnit.class))
+    ).thenAnswer(invocation -> {
+      Thread.sleep(0);
+      return commandResponseOk;
+    });
+
+    commander.createSession(session);
+    Thread.sleep(25);
+    commander.deleteSession(session);
+
+    verify(commandInterface, timeout(5000).times(1))
+        .sendRequest(eq(commandRequestCreate), anyLong(), any(TimeUnit.class));
+    verify(commandInterface, timeout(5000).times(0))
+        .sendRequest(eq(commandRequestDelete), anyLong(), any(TimeUnit.class));
   }
 }
