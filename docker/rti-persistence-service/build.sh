924e3858c207de1bfd74148b71e10c8c76560979
Andreas Guther <andreas@guther.net>
2018-08-05 20:30:18 +0200

    Added docker image for RTI Persistence Service

---
+++ docker/rti-persistence-service/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# copy needed files
+cp -f ./../rpms/rti-connext-dds-53-persistence-service-5.3.1.0-2.x86_64.rpm .
+
+# start build of docker file
+docker build -t rti-persistence-service:5.3.1 .
+
+# clean up files
+rm -f *.rpm
+
+# save docker image
+docker save -o rti-persistence-service-5.3.1.tar rti-persistence-service:5.3.1
+
+# gzip archive
+gzip -f rti-persistence-service-5.3.1.tar
