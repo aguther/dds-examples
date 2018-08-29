69cd7dde034f6970f47b5802fe19a441e566e1be
Andreas Guther <andreas@guther.net>
2018-08-29 18:09:39 +0200

    Updated to rpm revision 3

--- docker/rti-persistence-service/build.sh
+++ docker/rti-persistence-service/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # copy needed files
-cp -f ./../rpms/rti-connext-dds-53-persistence-service-5.3.1.0-2.x86_64.rpm .
+cp -f ./../rpms/rti-connext-dds-53-persistence-service-5.3.1.0-3.x86_64.rpm .
 
 # start build of docker file
 docker build -t rti-persistence-service:5.3.1 .
