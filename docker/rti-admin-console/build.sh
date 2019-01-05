fda2a6d234c7fc080cac64eb73c9bbc73f066650
Andreas Guther <andreas@guther.net>
2019-01-05 10:08:28 +0100

    Added docker image with RTI Admin Console

---
+++ docker/rti-admin-console/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# copy needed files
+cp -f ./../rpms/rti-connext-dds-53-admin-console-5.3.1.14-1.x86_64.rpm .
+cp -f ./../rpms/rti-connext-dds-53-license-5.3.1.14-1.x86_64.rpm .
+
+# start build of docker file
+docker build -t rti-admin-console:5.3.1 .
+
+# clean up files
+rm -f *.rpm
+
+# save docker image
+docker save -o rti-admin-console--5.3.1.tar rti-admin-console:5.3.1
+
+# gzip archive
+gzip -f rti-admin-console--5.3.1.tar
