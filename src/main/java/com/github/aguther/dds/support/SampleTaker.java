629d9726c0d50ec01c92e9984c5bd21fc4e7c562
Andreas Guther <andreas@guther.net>
2019-07-09 18:11:10 +0200

    Moved support classes to own package

---
+++ src/main/java/com/github/aguther/dds/support/SampleTaker.java
@@ -0,0 +1,82 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Andreas Guther
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.aguther.dds.support;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.rti.dds.infrastructure.ResourceLimitsQosPolicy;
+import com.rti.dds.subscription.DataReader;
+import com.rti.dds.subscription.ReadCondition;
+import com.rti.dds.subscription.SampleInfoSeq;
+import com.rti.dds.util.LoanableSequence;
+import java.util.List;
+
+public class SampleTaker<T> implements DataReaderWatcherExecutor<T> {
+
+  private List<T> sampleSeq;
+  private SampleInfoSeq sampleInfoSeq;
+
+  @SuppressWarnings("unchecked")
+  public SampleTaker(
+    LoanableSequence sampleSeq
+  ) {
+    checkNotNull(sampleSeq);
+
+    this.sampleSeq = sampleSeq;
+    this.sampleInfoSeq = new SampleInfoSeq();
+  }
+
+  public void execute(
+    DataReader dataReader,
+    ReadCondition readCondition,
+    OnDataAvailableListener<T> listener
+  ) {
+    try {
+      // take data
+      dataReader.take_w_condition_untyped(
+        sampleSeq,
+        sampleInfoSeq,
+        ResourceLimitsQosPolicy.LENGTH_UNLIMITED,
+        readCondition
+      );
+
+      // iterate over data
+      for (int i = 0; i < sampleSeq.size(); i++) {
+        listener.onDataAvailable(
+          dataReader,
+          sampleSeq.get(i),
+          sampleInfoSeq.get(i)
+        );
+      }
+
+    } finally {
+      // return data
+      dataReader.return_loan_untyped(
+        sampleSeq,
+        sampleInfoSeq
+      );
+    }
+  }
+}
