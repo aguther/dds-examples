d19b0c4962bd398da9663e20f3e8d673f0bb9839
Andreas Guther <andreas@guther.net>
2019-07-14 23:19:30 +0200

    Added version tag from git to dds-examples docker container

--- docker/dds-examples/build.sh
+++ docker/dds-examples/build.sh
@@ -13,14 +13,17 @@ popd
 cp -f ./../../build/libs/*all.jar .
 cp -f ./../rpms/rti-connext-dds-60-runtime-pro-x64Linux3gcc4.8.2-6.0.0.0-1.x86_64.rpm .
 
+# set version
+VERSION=6.0.0-$(git describe --always --dirty)
+
 # start build of docker file
-docker build -t dds-examples:latest .
+docker build -t dds-examples:"${VERSION}" .
 
 # clean up files
 rm -f *.jar *.xml *.rpm
 
 # save docker image
-docker save -o dds-examples--latest.tar dds-examples:latest
+docker save -o dds-examples--"${VERSION}".tar dds-examples:"${VERSION}"
 
 # gzip archive
-gzip -f dds-examples--latest.tar
+gzip -f dds-examples--"${VERSION}".tar
